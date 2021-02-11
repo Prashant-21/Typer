de66919a2cad4e16a6931f5720b5e94ed27bacb3
Prashant <prashantamoli@outlook.com>
2021-02-11 12:33:04 +0530

    ➕API🔗Updated

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ var __awaiter =
       step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
   };
-const RANDOM_QUOTE_API_URL = "http://api.quotable.io/random";
+const RANDOM_QUOTE_API_URL = "https://api.quotable.io/random";
 const quoteDisplayElement = document.getElementById("quoteDisplay");
 const quoteInputElement = document.getElementById("quoteInput");
 const timerElement = document.getElementById("timer");
